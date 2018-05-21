f203be1c2c88a00c559e367ac6f3eedb61b40854
Hanyoung Jeong <8735132@gmail.com>
2018-05-21 23:47:41 +0900

    nodemon 추가

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,24 @@
 import * as express from 'express';
 import * as bodyParser from 'body-parser';
 import * as logger from 'morgan';
+// import { createConnection } from 'typeorm';
 
 // router
 import ApiRouter from './routes';
 
+// createConnection({
+// 	type: 'mysql',
+// 	host: 'localhost',
+// 	port: 3306,
+// 	username: 'root',
+// 	password: '12341234',
+// 	database: 'blockchain',
+// 	synchronize: true,
+// 	logging: false,
+// 	entities: [ 'src/entity/**/*.ts' ],
+// 	migrations: [ 'src/migration/**/*.ts' ],
+// 	subscribers: [ 'src/subscriber/**/*.ts' ]
+// }).then(() => {
 const app: express.Application = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -14,3 +28,4 @@ app.use(logger('dev'));
 app.use('/api/v1', ApiRouter);
 
 app.listen(4000, () => console.log('Server is listening on port 4000'));
+// });
