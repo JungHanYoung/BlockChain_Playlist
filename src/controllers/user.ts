f510a0e90a74e2f562bfcca888b3b1e150323147
Hanyoung Jeong <8735132@gmail.com>
2018-05-22 00:17:50 +0900

    유저 CRUD 완료

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,11 +1,53 @@
 import { Request, Response } from 'express';
+import { User } from '../models/user';
 
-export const allUsers = (_: Request, res: Response) => res.send('allUsers');
+export const getAllUsers = async (_: Request, res: Response) => {
+	const allUsers = await User.find({});
+	if (allUsers.length <= 0) {
+		res.send('User is nothing');
+	}
+	res.json(allUsers);
+};
 
-export const getUserById = (req: Request, res: Response) => res.send('getUserById ' + req.params.id);
+export const getUserById = async (req: Request, res: Response) => {
+	const { id } = req.params;
+	const user = await User.findOne(id);
+	user ? res.json(user) : res.send('user is not found');
+};
 
-export const addUser = (_: Request, res: Response) => res.send('addUser');
+export const addUser = async (req: Request, res: Response) => {
+	const { email, password } = req.body;
+	const user = User.create({
+		email,
+		password
+	});
+	await user.save();
+	res.json({
+		success: true
+	});
+};
 
-export const updateUser = (req: Request, res: Response) => res.send('updateUser ' + req.params.id);
+export const updateUser = async (req: Request, res: Response) => {
+	const { id } = req.params;
+	const { email, password } = req.body;
+	const user = await User.findOne(id);
+	if (user) {
+		user.email = email;
+		user.password = password;
+		await user.save();
+		res.json({ success: true });
+	} else {
+		res.send('update fail');
+	}
+};
 
-export const deleteUser = (req: Request, res: Response) => res.send('deleteUser ' + req.params.id);
+export const deleteUser = async (req: Request, res: Response) => {
+	const { id } = req.params;
+	const user = await User.findOne(id);
+	if (user) {
+		await user.remove();
+		res.json({ success: true });
+	} else {
+		res.send('user is not found');
+	}
+};
--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,9 +1,9 @@
 import * as express from 'express';
-import { allUsers, addUser, getUserById, updateUser, deleteUser } from '../controllers/user';
+import { getAllUsers, addUser, getUserById, updateUser, deleteUser } from '../controllers/user';
 
 const router: express.Router = express.Router();
 
-router.route('/').get(allUsers).post(addUser);
+router.route('/').get(getAllUsers).post(addUser);
 
 router.route('/:id').get(getUserById).put(updateUser).delete(deleteUser);
 
